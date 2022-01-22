1c264b5a1a2683f080dbcc1aae949029a1251d21
dapetri <dapetri0@gmail.com>
2022-01-22 19:41:30 +0100

    continuing implementation

--- src/data/process_raw_dataset.py
+++ src/data/process_raw_dataset.py
@@ -19,6 +19,9 @@ default_file_directory = os.path.join(
     '..',
     'data',
 )
+default_usecols = [
+    'time', 'close'
+]
 
 
 class RawDatasetProcessor:
@@ -28,12 +31,14 @@ class RawDatasetProcessor:
         granularity: int = default_granularity,
         file_directory: str = default_file_directory,
         indicators: list = default_indicators,
+        usecols: list = default_usecols,
     ) -> None:
         self.crypto_currencies = crypto_currencies
         self.granularity = granularity
         self.file_directory = file_directory
         self.indicators = indicators
         self.base_currency = 'EUR'
+        self.usecols = usecols
 
     def process_raw_datasets(self):
         pairs = []
@@ -49,13 +54,39 @@ class RawDatasetProcessor:
                 cryp = pair.replace(coin, '').replace('-', '')
                 if cryp not in seen and cryp in crypto_currencies:
                     pairs.append(pair)
-        files = []
+        dfiles = []
         for dir_path, _, file_names in os.walk(self.file_directory + '/raw/'):
             for file_name in file_names:
                 if file_name.split('--')[0] in pairs:
                     pairs.remove(file_name.split('--')[0])
-                    files.append(pd.read_csv(dir_path + file_name))
+                    dfiles.append(
+                        pd.read_csv(
+                            dir_path + file_name,
+                            index_col=0,
+                            usecols=self.usecols,
+                        )
+                    )
         assert(len(pairs) == 0)
+
+        df = dfiles[0]
+        for dfile in dfiles[1:]:
+            df = df.merge(
+                right=dfile,
+                how='inner',
+                on='time',
+            )
+        print(df)
+        print(df.index[0])
+
+    def make_time_continuous(
+        self,
+        df: pd.DataFrame,
+    ):
+        for i in range(df.shape[0] - 1):
+            t1 = df.index[i]
+            t2 = df.index[i+1]
+
+        # TODO: implement checker if time dif bigger than gran
 
 
 def main():
