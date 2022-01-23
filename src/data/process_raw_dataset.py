360b4d3c86d66fe02f60a0efde0b3426edfd0a45
dapetri <dapetri0@gmail.com>
2022-01-23 16:07:36 +0100

    finishing data processor

--- src/data/process_raw_dataset.py
+++ src/data/process_raw_dataset.py
@@ -7,7 +7,7 @@ from numpy import empty
 import pandas as pd
 
 
-default_granularity = 60
+default_accepted_error = 60
 default_crypto_currencies = [
     'BTC', 'ETH',
 ]
@@ -29,13 +29,13 @@ class RawDatasetProcessor:
     def __init__(
         self,
         crypto_currencies: list = default_crypto_currencies,
-        granularity: int = default_granularity,
+        accepted_error: int = default_accepted_error,
         file_directory: str = default_file_directory,
         indicators: list = default_indicators,
         usecols: list = default_usecols,
     ) -> None:
         self.crypto_currencies = crypto_currencies
-        self.granularity = granularity
+        self.accepted_error = accepted_error
         self.file_directory = file_directory
         self.indicators = indicators
         self.base_currency = 'EUR'
@@ -76,33 +76,107 @@ class RawDatasetProcessor:
                 how='inner',
                 on='time',
             )
-        df = self.make_time_continuous(df)
-        print(df)
+        # self._error_count(
+        #     df=df,
+        #     accepted_error=[60, 120, 180, 240, 300]
+        # )
+
+        # self._biggest_error(df)
+
+        file_name = '-'.join(crypto_currencies) + '--g' + \
+            str(self.accepted_error) + '.csv'
+        file_name = self.file_directory + '/processed/' + file_name
+        df.to_csv(file_name)
 
     def make_time_continuous(
         self,
         df: pd.DataFrame,
     ):
+        last = 0
         for i in range(df.shape[0] - 1):
             d_earlier = self.transform_date(df.index[i])
             d_later = self.transform_date(df.index[i+1])
             if not self.date_continuous(
                 date_earlier=d_earlier,
                 date_later=d_later,
+                accepted_error=self.accepted_error,
             ):
-                df.drop(index=i)
-        return df
+                last = i
+        print(df.index[range(last)])
+        return df.drop(
+            df.index[range(last)],
+        )
 
         # TODO: implement checker if time dif bigger than gran
 
-    def transform_date(self, date: str) -> str:
+    def _biggest_error(
+        self,
+        df: pd.DataFrame,
+    ):
+        error_diff = 0
+        for i in range(df.shape[0] - 1):
+            d_earlier = self.transform_date(df.index[i])
+            d_later = self.transform_date(df.index[i+1])
+            diff = self.time_difference(d_earlier, d_later)
+            if diff > error_diff:
+                error_diff = diff
+        print(error_diff)
+
+    def _error_count(
+        self,
+        df: pd.DataFrame,
+        accepted_error: list,
+    ) -> None:
+        print('ERRORS:')
+        for error in accepted_error:
+            count = 0
+            last = ''
+            for i in range(df.shape[0] - 1):
+                d_earlier = self.transform_date(df.index[i])
+                d_later = self.transform_date(df.index[i+1])
+                if not self.date_continuous(
+                    date_earlier=d_earlier,
+                    date_later=d_later,
+                    accepted_error=error,
+                ):
+                    count += 1
+                    last = d_later
+            print(str(error) + 'sec: ' + str(count) + ', last at: ' + last)
+
+    def transform_date(
+        self,
+        date: str,
+    ) -> str:
         return date.replace(' ', '-').replace(':', '-')[:-3]
+
+    def time_difference(
+        self,
+        date_earlier: str,
+        date_later: str,
+    ) -> int:
+        d_earlier = [int(i) for i in date_earlier.split(sep='-')]
+        d_later = [int(i) for i in date_later.split(sep='-')]
+        dt_earlier = datetime(
+            year=d_earlier[0],
+            month=d_earlier[1],
+            day=d_earlier[2],
+            hour=d_earlier[3],
+            minute=d_earlier[4]
+        )
+        dt_later = datetime(
+            year=d_later[0],
+            month=d_later[1],
+            day=d_later[2],
+            hour=d_later[3],
+            minute=d_later[4]
+        )
+        return (dt_later - dt_earlier).total_seconds()
 
     def date_continuous(
         self,
         date_earlier: str,
         date_later: str,
-
+        accepted_error: int,
     ) -> bool:
         d_earlier = [int(i) for i in date_earlier.split(sep='-')]
         d_later = [int(i) for i in date_later.split(sep='-')]
@@ -121,7 +195,7 @@ class RawDatasetProcessor:
             minute=d_later[4]
         )
 
-        return dt_earlier + timedelta(seconds=self.granularity) == dt_later
+        return dt_earlier + timedelta(seconds=accepted_error) >= dt_later
 
 
 def main():
